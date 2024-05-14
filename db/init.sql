c2de9077c85629cfcd4b178551bc983c9fbf5804
chookopaika <127122973+chookopaika@users.noreply.github.com>
2024-05-14 18:08:52 +0300

    Delete db directory

--- db/init.sql
+++
@@ -1,9 +0,0 @@
-create table if not exists phone_number_table (
-  id serial primary key,
-  phone_number varchar (30) not null
-);
-
-create table if not exists emails_table (
-  id serial primary key,
-  email varchar (40) not null
-);
